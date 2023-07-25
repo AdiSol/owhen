ac5d08dd0f62efebb3eb9e8d45548ddd6825666b
Adisol <adriennesoliven@gmail.com>
2023-07-25 17:56:46 +0800

    feat: basic routes setup in preparation, style: favicon change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+import { Routes, Route} from 'react-router-dom';
+import Layout from './Components/Layout';
+import HomePage from './Pages/HomePage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <header className='App-header'>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="" element={<HomePage/>}/>
+        </Route>
+      </Routes>
       </header>
-    </div>
+    </>
   );
 }
 
